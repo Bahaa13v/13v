b2c94d0064ea6055c705bf381d2d5adbfcc14fe2
Bahaa <121813996+bahaa13v@users.noreply.github.com>
2025-03-15 18:46:36 +0100

    Update arch.sh

--- src/shell/arch.sh
+++ src/shell/arch.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-sudo pacman -Sy
-sudo pacman -S git
-mkdir gits && cd gits
+mkdir ../gits && cd ../gits
 git clone https://github.com/bahaa13v/arch.git
 cd cd arch && chmod +x arch.sh
 bash arch.sh
