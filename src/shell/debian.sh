88e23c8b573ea364cda8d1cc364dff7b997ec139
Bahaa <121813996+bahaa13v@users.noreply.github.com>
2025-03-15 12:54:18 +0100

    Update debian.sh

--- src/shell/debian.sh
+++ src/shell/debian.sh
@@ -4,5 +4,5 @@ sudo apt update
 sudo apt install git
 mkdir gits && cd gits
 git clone https://github.com/bahaa13v/debian.git
-cd Debian && chmod +x Debian.sh
-bash Debian.sh
+cd debian && chmod +x debian.sh
+bash debian.sh
