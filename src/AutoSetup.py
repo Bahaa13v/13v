c143bdd3431401cf378d48e2a2c646c14c59e1b0
Bahaa <121813996+bahaa13v@users.noreply.github.com>
2025-03-15 12:48:03 +0100

    Update AutoSetup.py

--- src/AutoSetup.py
+++ src/AutoSetup.py
@@ -5,17 +5,17 @@ hos = bash.run(["cat", "/etc/hostname"], capture_output=True, text=True).stdout.
 if hos == "debian":
 
   bash.run(["chmod", "+x", "shell/deb.sh"])
-  bash.run(["bash", "shell/deb.sh"])
+  bash.run(["bash", "shell/debian.sh"])
 
 elif hos == "Arch":
 
   bash.run(["chmod", "+x", "shell/dot.sh"])
-  bash.run(["bash", "shell/dot.sh"])
+  bash.run(["bash", "shell/arch.sh"])
 
 elif hos == "archiso":
 
   bash.run(["chmod", "+x", "shell/arc.sh"])
-  bash.run(["bash", "shell/arc.sh"])
+  bash.run(["bash", "shell/archiso.sh"])
 
 else:
     print("bahaa support only debian and Arch")
