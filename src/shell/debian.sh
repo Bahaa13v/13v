c98b79ab4f6fb5ecf0a26029f6dba06e2f8fe9f8
Bahaa <121813996+bahaa13v@users.noreply.github.com>
2025-03-15 12:49:09 +0100

    Update and rename deb.sh to debian.sh

--- src/shell/debian.sh
+++ src/shell/debian.sh
@@ -3,6 +3,6 @@
 sudo apt update
 sudo apt install git
 mkdir gits && cd gits
-git clone https://github.com/Bahaa13v/Debian.git
+git clone https://github.com/bahaa13v/debian.git
 cd Debian && chmod +x Debian.sh
 bash Debian.sh
