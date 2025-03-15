92fb864c8aa0cbf288cbb5f18e9bfcc4ce59e37e
Bahaa <121813996+bahaa13v@users.noreply.github.com>
2025-03-15 12:55:41 +0100

    Update AutoSetup.py

--- src/AutoSetup.py
+++ src/AutoSetup.py
@@ -4,17 +4,17 @@ hos = bash.run(["cat", "/etc/hostname"], capture_output=True, text=True).stdout.
   
 if hos == "debian":
 
-  bash.run(["chmod", "+x", "shell/deb.sh"])
+  bash.run(["chmod", "+x", "shell/debian.sh"])
   bash.run(["bash", "shell/debian.sh"])
 
 elif hos == "Arch":
 
-  bash.run(["chmod", "+x", "shell/dot.sh"])
+  bash.run(["chmod", "+x", "shell/arch.sh"])
   bash.run(["bash", "shell/arch.sh"])
 
 elif hos == "archiso":
 
-  bash.run(["chmod", "+x", "shell/arc.sh"])
+  bash.run(["chmod", "+x", "shell/archiso.sh"])
   bash.run(["bash", "shell/archiso.sh"])
 
 else:
