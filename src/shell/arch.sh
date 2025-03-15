198bf7fdb9661cc82e49b9dd1659a6112a6af25a
Bahaa <121813996+bahaa13v@users.noreply.github.com>
2025-03-15 12:49:53 +0100

    Update and rename dot.sh to arch.sh

--- src/shell/arch.sh
+++ src/shell/arch.sh
@@ -3,6 +3,6 @@
 sudo pacman -Sy
 sudo pacman -S git
 mkdir gits && cd gits
-git clone https://github.com/Bahaa13v/Dotfiles.git
+git clone https://github.com/bahaa13v/arch.git
 cd cd Dotfiles && chmod +x Dotfiles.sh
 bash Dotfiles.sh
