431398f2355319246c75807f32a430667701a503
Bahaa <121813996+bahaa13v@users.noreply.github.com>
2025-03-17 17:24:27 +0100

    Update arch.sh

--- src/hypr/arch.sh
+++ src/hypr/arch.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
+ls
 #sudo pacman -S --noconfirm --needed xorg xorg-drivers xorg-xwayland wayland base-devel cmake ninja meson hyprland wlroots
 #sudo pacman -R --noconfirm hyprland
-ls
 #bash hypr.sh
