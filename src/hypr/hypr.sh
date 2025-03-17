37468cecfb4418633789920d9377ee9161224b47
Bahaa <121813996+bahaa13v@users.noreply.github.com>
2025-03-17 17:33:55 +0100

    Delete src/hypr/hypr.sh

--- src/hypr/hypr.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git clone --recursive https://github.com/hyprwm/Hyprland
-
-cd Hyprland
-make -j2 legacyrenderer && sudo cp ./build/Hyprland /usr/bin && sudo cp ./example/hyprland.desktop /usr/share/wayland-sessions
